9b17c2d2852ca57f3f136f4ed3c9c10668ed151f
Robert Austin <rob@jugglerdigital.com>
2019-09-20 16:29:42 +1000

    add filter counts

--- themes/jamstackthemes/assets/js/filter.js
+++ themes/jamstackthemes/assets/js/filter.js
@@ -13,10 +13,31 @@ var mixer = mixitup('.grids', {
     target: '.grid'
   },
   callbacks: {
-    onMixStart: function(state, futureState) {
-      let total = futureState.totalShow;
+    onMixEnd: function(state) {
+      let total = state.totalShow;
       let count = document.querySelector('.count-number');
       count.textContent = total
+      updateFilterCounts(state.matching);
     }
   }
 });
+
+
+function updateFilterCounts(themes) {
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]')
+
+  checkboxes.forEach((checkbox) => {
+    var filterValue = checkbox.value.slice(1);
+    var filterCount = themes.reduce((sum, theme) => {
+      let themeClasses = theme.className.trim().split(' ');
+      let matchedClasses = themeClasses.filter((className) => {
+        return filterValue == className;
+      });
+      if (matchedClasses.length > 0) {
+        return sum += 1
+      }
+      return sum;
+    }, 0);
+    document.querySelector(`#filter-count-${filterValue}`).innerText = filterCount
+  });
+}
