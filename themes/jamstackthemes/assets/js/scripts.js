51686fbb80985898dd5e07f76e5fa9f53c8bbccb
Eric Jinks <ericjinks@gmail.com>
2019-10-21 12:10:01 +1000

    Improve filter-list accessibility

--- themes/jamstackthemes/assets/js/scripts.js
+++ themes/jamstackthemes/assets/js/scripts.js
@@ -1,7 +1,21 @@
-document.querySelectorAll(".toggle-icon").forEach((title) => {
-  title.addEventListener('click', (e) => {
-    e.currentTarget.classList.toggle('closed');
-    e.currentTarget.parentNode.parentNode.classList.toggle('closed');
+function toggleFilterList(element) {
+  // Check aria-expanded state
+  const pressed = (element.getAttribute("aria-expanded") === "true");
+  element.setAttribute("aria-expanded", !pressed);
+  element.classList.toggle('closed');
+  element.parentNode.parentNode.classList.toggle('closed');
+}
+
+document.querySelectorAll(".toggle-icon").forEach((toggleIcon) => {
+  toggleIcon.addEventListener('click', (e) => {
+    toggleFilterList(e.currentTarget);
+  })
+  toggleIcon.addEventListener('keydown', (e) => {
+    if (e.key === " " || e.key === "Enter" || e.key === "Spacebar") { // "Spacebar" for IE11 support
+      // Prevent the default action to stop scrolling when space is pressed
+      e.preventDefault();
+      toggleFilterList(event.target);
+    }
   })
 })
 
