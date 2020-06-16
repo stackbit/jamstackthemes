3a2b0f9de49833a56e55239bc20711c4974fd971
Robert Austin <rob@jugglerdigital.com>
2020-06-16 16:31:11 +1000

    add analytics

---
+++ themes/jamstackthemes/assets/js/scripts.js
@@ -0,0 +1,16 @@
+document.querySelectorAll(".theme").forEach((theme) => {
+    const themeName = theme.getAttribute('data-name');
+    const githubButton = theme.querySelector('.theme-button-github');
+    const demoButton = theme.querySelector('.theme-button-demo');
+
+    githubButton.addEventListener('click', (e) => {
+        analytics.track("Theme Github Clicked", {
+            theme: themeName
+        });
+    });
+    demoButton.addEventListener('click', (e) => {
+        analytics.track("Theme Demo Clicked", {
+            theme: themeName
+        });
+    })
+});
